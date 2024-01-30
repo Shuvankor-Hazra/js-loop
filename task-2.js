1febb0f9e5c41bf1ed5c79084c5058805b50f43d
Shuvankr Hazra <shuvo.s.ovuhs@gmail.com>
2024-01-30 17:50:35 +0600

    2nd task added

---
+++ task-2.js
@@ -0,0 +1,31 @@
+// while // Subtask-1:odd
+let i = 61;
+while (i <= 100) {
+  // console.log(i);
+  if (i % 2 !== 0) {
+    console.log("ood numbers:", i);
+  }
+  i++;
+}
+
+// while // Subtask-2:even
+let ii = 78;
+while (ii <= 98) {
+  // console.log(i);
+  if (ii % 2 === 0) {
+    console.log("even numbers:", ii);
+  }
+  ii++;
+}
+
+// for // Subtask-1:odd
+let oddFor;
+for (let iii = 61; iii <= 100; iii += 2) {
+  console.log(iii);
+}
+
+// for // Subtask-2:even
+let evenFor;
+for (let iiii = 78; iiii <= 98; iiii += 2) {
+  console.log(iiii);
+}
